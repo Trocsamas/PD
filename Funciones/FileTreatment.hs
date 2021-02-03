5520159203acddcb4013c9c711761933adbbca62
Trocsamas <jesala31@gmail.com>
2021-02-03 20:32:29 +0100

    Lectura exitosa

---
+++ Funciones/FileTreatment.hs
@@ -0,0 +1,33 @@
+-- Este fichero contendrá todas las funciones relacionadas 
+-- con la lectura y escritura sobre ficheros
+-- =======================================================
+import System.Environment (getArgs)
+import System.Directory
+import Text.CSV
+import Text.Printf
+
+cargaDatos = do  
+        let fileName = "./PF.dat"
+        dat <- readFile fileName
+        let lineas = lines dat
+        return (aNumero (datos lineas)) 
+
+
+split :: Eq a => a -> [a] -> [[a]]
+split x y = func x y [[]]
+    where
+        func x [] z = reverse $ map (reverse) z
+        func x (y:ys) (z:zs) = if y==x then 
+            func x ys ([]:(z:zs)) 
+        else 
+            func x ys ((y:z):zs)
+
+datos lineas = foldr (\palabra js-> split '\t' palabra:js) [] lineas
+
+aNumero :: [[String]] -> [(Double, Double)]
+aNumero [] = []
+aNumero (x:xss) = (read (x!!0),read (x!!1)):aNumero xss
+
+
+-- F por Windows
+-- plotDots [Key Nothing, XRange (-0.1,1.1), YRange(-1.1,1.1)] a
